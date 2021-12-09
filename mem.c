546d7d58a1dfb300f28a283d608b1bfb894b1888
Mattéo Decorsaire <matteo.decorsaire@etu.univ-grenoble-alpes.fr>
2021-12-09 15:44:33 +0100

    add mem_init and mem_show

--- mem.c
+++ mem.c
@@ -23,79 +23,95 @@
    Elle peut bien évidemment être complétée
 */
 struct allocator_header {
-        size_t memory_size;
-	mem_fit_function_t *fit;
+    size_t memory_size;
+    mem_fit_function_t *fit;
 };
+
 
 /* La seule variable globale autorisée
  * On trouve à cette adresse le début de la zone à gérer
  * (et une structure 'struct allocator_header)
  */
-static void* memory_addr;
+static void *memory_addr;
 
 static inline void *get_system_memory_addr() {
-	return memory_addr;
+    return memory_addr;
 }
 
 static inline struct allocator_header *get_header() {
-	struct allocator_header *h;
-	h = get_system_memory_addr();
-	return h;
+    struct allocator_header *h;
+    h = get_system_memory_addr();
+    return h;
+}
+
+static inline struct fb *get_fb_head() {
+    return (struct fb *) (get_system_memory_addr() + sizeof(struct allocator_header));
 }
 
 static inline size_t get_system_memory_size() {
-	return get_header()->memory_size;
-}
-
-
-struct fb {
-	size_t size;
-	struct fb* next;
-	/* ... */
-};
-
-
-void mem_init(void* mem, size_t taille)
-{
-        memory_addr = mem;
-        *(size_t*)memory_addr = taille;
-	/* On vérifie qu'on a bien enregistré les infos et qu'on
-	 * sera capable de les récupérer par la suite
-	 */
-	assert(mem == get_system_memory_addr());
-	assert(taille == get_system_memory_size());
-	/* ... */
-	mem_fit(&mem_fit_first);
-}
-
-void mem_show(void (*print)(void *, size_t, int)) {
-	/* ... */
-	while (/* ... */ 0) {
-		/* ... */
-		print(/* ... */NULL, /* ... */0, /* ... */0);
-		/* ... */
-	}
+    return get_header()->memory_size;
 }
 
 void mem_fit(mem_fit_function_t *f) {
-	get_header()->fit = f;
+    get_header()->fit = f;
+}
+
+void mem_size(size_t size) {
+    get_header()->memory_size = size;
+}
+
+// Voir nos schémas pour comprendre notre utilisation de fb
+struct fb {
+    size_t size;
+    struct fb *next;
+};
+
+
+void mem_init(void *mem, size_t taille) {
+    // On met en place fb
+    memory_addr = mem;
+    *(size_t *) memory_addr = taille;
+    /* On vérifie qu'on a bien enregistré les infos et qu'on
+     * sera capable de les récupérer par la suite
+     */
+    assert(mem == get_system_memory_addr());
+    assert(taille == get_system_memory_size());
+
+    //On met en place allocator header
+    mem_size(taille + sizeof(struct allocator_header));
+    mem_fit(&mem_fit_first);
+}
+
+void mem_show(void (*print)(void *, size_t, int)) {
+    struct fb *cell = get_fb_head();
+    int written;
+    while (cell != NULL) {
+        if (cell->next == NULL) {
+            written = 0;
+        } else {
+            struct fb *next = cell->next;
+            written = next - cell - cell->size;
+        }
+        print(cell, cell->size, written != cell->size);
+        cell = cell->next;
+    }
 }
 
 void *mem_alloc(size_t taille) {
-	/* ... */
-	__attribute__((unused)) /* juste pour que gcc compile ce squelette avec -Werror */
-	struct fb *fb=get_header()->fit(/*...*/NULL, /*...*/0);
-	/* ... */
-	return NULL;
+    /* ... */
+    __attribute__((unused)) /* juste pour que gcc compile ce squelette avec -Werror */
+    struct fb *fb = get_header()->fit(/*...*/NULL, /*...*/0);
+    /* ... */
+    return NULL;
 }
 
 
-void mem_free(void* mem) {
+void mem_free(void *mem) {
 }
 
 
-struct fb* mem_fit_first(struct fb *list, size_t size) {
-	return NULL;
+struct fb *mem_fit_first(struct fb *list, size_t size) {
+    return NULL;
 }
 
 /* Fonction à faire dans un second temps
@@ -106,20 +122,20 @@ struct fb* mem_fit_first(struct fb *list, size_t size) {
  * (ou en discuter avec l'enseignant)
  */
 size_t mem_get_size(void *zone) {
-	/* zone est une adresse qui a été retournée par mem_alloc() */
+    /* zone est une adresse qui a été retournée par mem_alloc() */
 
-	/* la valeur retournée doit être la taille maximale que
-	 * l'utilisateur peut utiliser dans cette zone */
-	return 0;
+    /* la valeur retournée doit être la taille maximale que
+     * l'utilisateur peut utiliser dans cette zone */
+    return 0;
 }
 
 /* Fonctions facultatives
  * autres stratégies d'allocation
  */
-struct fb* mem_fit_best(struct fb *list, size_t size) {
-	return NULL;
+struct fb *mem_fit_best(struct fb *list, size_t size) {
+    return NULL;
 }
 
-struct fb* mem_fit_worst(struct fb *list, size_t size) {
-	return NULL;
+struct fb *mem_fit_worst(struct fb *list, size_t size) {
+    return NULL;
 }
